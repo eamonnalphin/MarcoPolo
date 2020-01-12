f98ade17ef3c60fc0c51df4839e16cee3380dc97
Eamonn <eamonnalphin@gmail.com>
2020-01-11 18:08:10 -0800

    added DB creation code file

---
+++ DBCreation.sql
@@ -0,0 +1,53 @@
+/*
+CREATE TABLE ADMINLOGIN(
+    ID int not null AUTO_INCREMENT,
+    USERNAME varchar(255) not null,
+    PASSWORD varchar(255) not null,
+    SALT varchar(255) not null,
+    PRIMARY KEY (ID)
+);
+
+insert into ADMINLOGIN (
+    `PASSWORD`,
+    SALT,
+    USERNAME)
+values ('3c3b237b526da4d5295059ee6549e4760bbe2cf4c8870b812a33f03b8c5dac10acc38237169771040d3be4435d3466b7a6c9815d304ce91ee4638d648b53858b','3268c18f44269202589137af9a7f2f95','eamonnalphin@gmail.com')
+;
+
+*/
+
+/*
+Contains the data for a specific event, like Burning Man
+*/
+CREATE TABLE EVENT (
+  ID int PRIMARY KEY AUTO_INCREMENT,
+  NAME varchar(255),
+  DESCRIPTION varchar(255),
+  ADMINID int,
+  FOREIGN KEY (ADMINID) REFERENCES ADMINLOGIN(ID)
+);
+
+/*
+Contains data for a day of the event, like Day 1 of burning man
+*/
+CREATE TABLE EVENTDAY (
+  ID int PRIMARY KEY AUTO_INCREMENT,
+  EVENTID int,
+  ATTENDEES int,
+  TIMESTART datetime,
+  TIMEEND datetime,
+  NOTE VARCHAR(255),
+  FOREIGN KEY (EVENTID) REFERENCES EVENT(ID)
+);
+
+/*
+Contains entries for a day of an event, like 01:10:20:30:30
+*/
+CREATE TABLE EVENTDAYENTRY (
+  ID int PRIMARY KEY AUTO_INCREMENT,
+  TIMESTAMP datetime,
+  MAC varchar(17),
+  WIFI varchar(255),
+  EVENTDAYID int,
+  FOREIGN KEY (EVENTDAYID) REFERENCES EVENTDAY(ID)
+);
