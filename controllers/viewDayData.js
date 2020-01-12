ac50d3757c9b274c1e025881ed25d32a924da359
skylerdev <Skyler2@mac.com>
2020-01-12 12:21:00 -0800

    experimental graphing

--- controllers/viewDayData.js
+++ controllers/viewDayData.js
@@ -53,7 +53,54 @@ exports.renderForID = (req, res, next) =>{
  **/
 function renderPage(req,res) {
 
+    
+
     appModel.getAllEventDayEntriesForDayID(req.body.eventDayID,function(theseRows){
+
+        let i = 0;
+        
+        let macs = [];
+        let starts = [];
+        let ends = [];
+
+        for(i = 0; i < theseRows.length; i++){
+            let cur = theseRows[i];
+            if(macs.includes(cur.MAC)){
+                continue;
+            }
+
+            macs.push(cur.MAC);
+
+            starts.push(cur.timestamp);
+            
+            let last = cur.timestamp;
+            console.log(i);
+    
+            for(let j = i; j < theseRows.length; j++){
+                console.log(j);
+                console.log(theseRows[j]);
+
+                if(theseRows[j].MAC == cur.MAC){
+                    last = theseRows[j].timestamp;
+                }
+            }
+            ends.push(last);
+        }
+
+        let objs = [];
+        let k;
+        for(k = 0; k < macs.length; k++){
+            objs[k] = {
+                mac: macs[k],
+                start: starts[k],
+                end: ends[k]
+            }
+        }
+
+        console.log(objs);
+
+
+
         res.render('viewDayData', {
             eventName: req.body.eventName,
             startTime: req.body.startTime,
