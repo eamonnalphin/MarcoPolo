bc1c76375b6ab1b7bc49c64b96fb49fd9941d336
Eamonn <eamonnalphin@gmail.com>
2020-01-12 08:56:39 -0800

    bugs

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,20 @@
 
 
 $(document).ready(function(){
-    $("#registerBtn").attr("disabled", true)
+    $("#registerBtn").prop("disabled",true)
 
     $('#confirmPassword').focusout(function(){
-        var pass = $('#password').val();
+        var pass = $('#userPassword').val();
         var pass2 = $('#confirmPassword').val();
-        if(pass != pass2){
+
+        if(pass.trim() == "" || pass2.trim() == ""){
+            $("#errorMessage").text("Password cannot be empty.")
+        }else if(pass == pass2){
+            $("#registerBtn").prop("disabled",false);
+        }else{
             console.log("passwords don't match")
             $("#errorMessage").text("Passwords do not match.")
-            $("#registerBtn").attr("disabled", true)
-        }else{
-            $("#registerBtn").attr("disabled", false)
+            $("#registerBtn").prop("disabled",true)
         }
 
     });
